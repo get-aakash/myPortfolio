daa6d5326f0ad56205dbe40273cb6d4ffbdb1e1d
Aakash Acharya <mail.aakash108@gmail.com>
2025-05-21 23:49:12 +1000

    Carousel image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
+import project1 from '../assets/project1.png'
+import project2 from '../assets/project2.png'
 
 const Projects = () => {
   return (
@@ -6,7 +9,32 @@ const Projects = () => {
       <div className="heading-line">
         <h1>/ pet projects</h1>
         <div className="line"></div>
-      </div>
+        </div>
+        <div className="project-slide">
+          <div className="my-carousel">
+          <Carousel>
+            <Carousel.Item>
+              <img className=" project d-block w-100 h-100" src={project1} alt="First slide" />
+              <Carousel.Caption>
+                <h3>First Slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className=" project d-block w-100 h-100" src={project2} alt="Second slide" />
+              <Carousel.Caption>
+                <h3>Second Slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="project-detail">
+
+        </div>
+
+        </div>
+
+        
+      
     </div>
   )
 }
